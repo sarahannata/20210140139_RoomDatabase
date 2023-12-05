426ce3044abe85b3db3d25aa372dd65f67998eb7
sarahannata <sarahannata@gmail.com>
2023-12-06 04:07:18 +0700

    add Column to Card at fun DataSiswa

--- app/src/main/java/com/example/pampraktikum7/ui/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/pampraktikum7/ui/halaman/HalamanHome.kt
@@ -1,10 +1,15 @@
 package com.example.pampraktikum7.ui.halaman
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.unit.dp
+import com.example.pampraktikum7.R
 import com.example.pampraktikum7.data.Siswa
 
 @Composable
@@ -16,6 +21,11 @@ fun DataSiswa(
         modifier = modifier,
         elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
     ){
-
+        Column (
+            modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_small))
+        ){
+            
+        }
     }
 }
