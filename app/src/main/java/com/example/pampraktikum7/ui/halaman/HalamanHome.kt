a1003f5b81e11873334c515710cb6643999babe3
sarahannata <sarahannata@gmail.com>
2023-12-06 04:24:52 +0700

    add LazyColumn at fun ListSiswa

--- app/src/main/java/com/example/pampraktikum7/ui/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/pampraktikum7/ui/halaman/HalamanHome.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Card
@@ -24,7 +26,17 @@ import com.example.pampraktikum7.data.Siswa
 fun ListSiswa(
     itemSiswa: List<Siswa>,
     modifier: Modifier = Modifier
-){}
+){
+    LazyColumn(modifier = Modifier){
+        items(items = itemSiswa, key = {it.id}){
+            person ->
+            DataSiswa(
+                siswa = person,
+                modifier = Modifier
+                    .padding(dimensionResource(id = R.dimen.padding_small)))
+        }
+    }
+}
 
 @Composable
 fun DataSiswa(
