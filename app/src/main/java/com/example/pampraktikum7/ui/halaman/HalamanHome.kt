dcb10586e790c963d220e0a5ab390e78e10f8eb6
sarahannata <sarahannata@gmail.com>
2023-12-06 04:28:01 +0700

    add Column to fun BodyHome at HalamanHome.kt

--- app/src/main/java/com/example/pampraktikum7/ui/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/pampraktikum7/ui/halaman/HalamanHome.kt
@@ -16,11 +16,25 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.unit.dp
 import com.example.pampraktikum7.R
 import com.example.pampraktikum7.data.Siswa
+
+@Composable
+fun BodyHome(
+    itemSiswa: List<Siswa>,
+    modifier: Modifier=Modifier
+){
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ){
+
+    }
+}
 
 @Composable
 fun ListSiswa(
