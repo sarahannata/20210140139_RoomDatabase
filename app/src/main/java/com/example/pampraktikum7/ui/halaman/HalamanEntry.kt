f271413e27be7542999667fb4f8ba28d33232b76
sarahannata <sarahannata@gmail.com>
2023-12-06 02:05:35 +0700

    add fun FormInputSiswa to HalamanEntry.kt

---
+++ app/src/main/java/com/example/pampraktikum7/ui/halaman/HalamanEntry.kt
@@ -0,0 +1,14 @@
+package com.example.pampraktikum7.ui.halaman
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.pampraktikum7.model.DetailSiswa
+
+@Composable
+fun FormInputSiswa(
+ detailSiswa: DetailSiswa,
+ modifier: Modifier = Modifier,
+ onValueChange: (DetailSiswa) -> Unit = {},
+ enabled: Boolean = true
+){
+}
