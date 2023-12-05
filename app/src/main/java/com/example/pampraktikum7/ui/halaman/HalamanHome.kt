c948cd290b5288706104e4aefa14d48e54dc4dc7
sarahannata <sarahannata@gmail.com>
2023-12-06 04:03:16 +0700

    add fun DataSiswa at HalamanHome.kt

---
+++ app/src/main/java/com/example/pampraktikum7/ui/halaman/HalamanHome.kt
@@ -0,0 +1,13 @@
+package com.example.pampraktikum7.ui.halaman
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.pampraktikum7.data.Siswa
+
+@Composable
+fun DataSiswa(
+    siswa: Siswa,
+    modifier: Modifier = Modifier
+){
+
+}
