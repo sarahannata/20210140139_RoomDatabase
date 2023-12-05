eba35d7ec75b34ed67535ec69941611a2b2972ac
sarahannata <sarahannata@gmail.com>
2023-12-06 02:35:17 +0700

    add OutlinedTextField telpon to ColumnScope in fun FormInputSiswa

--- app/src/main/java/com/example/pampraktikum7/ui/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/pampraktikum7/ui/halaman/HalamanEntry.kt
@@ -3,6 +3,7 @@ package com.example.pampraktikum7.ui.halaman
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -10,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import com.example.pampraktikum7.R
 import com.example.pampraktikum7.model.DetailSiswa
 
@@ -37,6 +39,15 @@ fun FormInputSiswa(
    value = detailSiswa.alamat,
    onValueChange = {onValueChange(detailSiswa.copy(alamat = it))},
    label = { Text(stringResource(id = R.string.alamat))},
+   modifier = Modifier.fillMaxWidth(),
+   enabled = enabled,
+   singleLine = true
+  )
+  OutlinedTextField(
+   value = detailSiswa.telpon,
+   onValueChange = {onValueChange(detailSiswa.copy(telpon = it))},
+   keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+   label = { Text(stringResource(id = R.string.telpon))},
    modifier = Modifier.fillMaxWidth(),
    enabled = enabled,
    singleLine = true
