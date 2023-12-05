f9b6ef9792cf0de14e06947cdb0a2c5228a20cc8
sarahannata <sarahannata@gmail.com>
2023-12-06 04:04:39 +0700

    add Card to fun DataSiswa at HalamanHome.kt

--- app/src/main/java/com/example/pampraktikum7/ui/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/pampraktikum7/ui/halaman/HalamanHome.kt
@@ -1,7 +1,10 @@
 package com.example.pampraktikum7.ui.halaman
 
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.pampraktikum7.data.Siswa
 
 @Composable
@@ -9,5 +12,10 @@ fun DataSiswa(
     siswa: Siswa,
     modifier: Modifier = Modifier
 ){
+    Card (
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
+    ){
 
+    }
 }
