2f67118fe50dec39a593ba644c56118c285368cb
sarahannata <sarahannata@gmail.com>
2023-12-06 02:18:59 +0700

    add OutlinedTextField nama to ColumnScope in fun FormInputSiswa

--- app/src/main/java/com/example/pampraktikum7/ui/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/pampraktikum7/ui/halaman/HalamanEntry.kt
@@ -2,12 +2,18 @@ package com.example.pampraktikum7.ui.halaman
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import com.example.pampraktikum7.R
 import com.example.pampraktikum7.model.DetailSiswa
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormInputSiswa(
  detailSiswa: DetailSiswa,
@@ -19,6 +25,13 @@ fun FormInputSiswa(
   modifier = modifier,
   verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
  ) {
-  
+  OutlinedTextField(
+   value = detailSiswa.nama,
+   onValueChange = {onValueChange(detailSiswa.copy(nama = it))},
+   label = { Text(stringResource(id = R.string.nama))},
+   modifier = Modifier.fillMaxWidth(),
+   enabled = enabled,
+   singleLine = true
+  )
  }
 }
