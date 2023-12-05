b130c9fbb6861727fbcd8b82b1fceb313eac7d46
sarahannata <sarahannata@gmail.com>
2023-12-06 03:29:58 +0700

    add fun SiswaTopAppBar at PengelolaHalaman.kt

---
+++ app/src/main/java/com/example/pampraktikum7/navigasi/PengelolaHalaman.kt
@@ -0,0 +1,19 @@
+package com.example.pampraktikum7.navigasi
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {}
+){
+
+}
