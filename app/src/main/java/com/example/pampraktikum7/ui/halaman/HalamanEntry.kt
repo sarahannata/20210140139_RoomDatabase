51528dd95bb44e1ef8b524b4d2b2cc182b43a06c
sarahannata <sarahannata@gmail.com>
2023-12-06 02:10:29 +0700

    add Column to fun FormInputSiswa at HalamanEntry.kt

--- app/src/main/java/com/example/pampraktikum7/ui/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/pampraktikum7/ui/halaman/HalamanEntry.kt
@@ -1,7 +1,11 @@
 package com.example.pampraktikum7.ui.halaman
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import com.example.pampraktikum7.R
 import com.example.pampraktikum7.model.DetailSiswa
 
 @Composable
@@ -11,4 +15,10 @@ fun FormInputSiswa(
  onValueChange: (DetailSiswa) -> Unit = {},
  enabled: Boolean = true
 ){
+ Column(
+  modifier = modifier,
+  verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+ ) {
+  
+ }
 }
