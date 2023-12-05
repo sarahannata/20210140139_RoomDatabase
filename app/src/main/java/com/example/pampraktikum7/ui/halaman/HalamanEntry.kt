f98f7ca1e1c44a2c3c02ab673945f496a8d0287a
sarahannata <sarahannata@gmail.com>
2023-12-06 03:49:55 +0700

    add Scaffold, innerPadding, and modifier to fun EntrySiswaScreen

--- app/src/main/java/com/example/pampraktikum7/ui/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/pampraktikum7/ui/halaman/HalamanEntry.kt
@@ -4,7 +4,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -28,6 +30,7 @@ import com.example.pampraktikum7.model.PenyediaViewModel
 import com.example.pampraktikum7.model.UIStateSiswa
 import com.example.pampraktikum7.navigasi.DestinasiNavigasi
 import com.example.pampraktikum7.navigasi.SiswaTopAppBar
+import kotlinx.coroutines.launch
 
 object DestinasiEntry:DestinasiNavigasi{
  override val route = "item_entry"
@@ -41,7 +44,33 @@ fun EntrySiswaScreen(
  modifier: Modifier = Modifier,
  viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ){
-
+ val coroutineScope = rememberCoroutineScope()
+ val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+ Scaffold (
+  modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+  topBar ={
+   SiswaTopAppBar(
+    title = stringResource(DestinasiEntry.titleRes),
+    canNavigateBack = true,
+    scrollBehavior = scrollBehavior
+   )
+  }
+ ){
+  innerPadding -> EntrySiswaBody(
+  uiStateSiswa = viewModel.uiStateSiswa,
+  onSiswaValueChange = viewModel::updateUiState,
+  onSaveClick = {
+   coroutineScope.launch {
+    viewModel.saveSiswa()
+    navigateBack()
+   }
+  },
+   modifier = Modifier
+    .padding(innerPadding)
+    .verticalScroll(rememberScrollState())
+    .fillMaxWidth()
+   )
+ }
 }
 
 @Composable
