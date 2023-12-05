ee687122a06322624975c78597b58be0b88733e3
sarahannata <sarahannata@gmail.com>
2023-12-06 03:45:06 +0700

    add object DestinasiEntry to HalamanEntry.kt

--- app/src/main/java/com/example/pampraktikum7/ui/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/pampraktikum7/ui/halaman/HalamanEntry.kt
@@ -10,9 +10,13 @@ import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
@@ -22,13 +26,23 @@ import com.example.pampraktikum7.model.DetailSiswa
 import com.example.pampraktikum7.model.EntryViewModel
 import com.example.pampraktikum7.model.PenyediaViewModel
 import com.example.pampraktikum7.model.UIStateSiswa
+import com.example.pampraktikum7.navigasi.DestinasiNavigasi
+import com.example.pampraktikum7.navigasi.SiswaTopAppBar
 
+object DestinasiEntry:DestinasiNavigasi{
+ override val route = "item_entry"
+ override val titleRes = R.string.entry_siswa
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun EntrySiswaScreen(
  navigateBack: () -> Unit,
  modifier: Modifier = Modifier,
  viewModel: EntryViewModel = viewModel(factory = PenyediaViewModel.Factory)
-){}
+){
+
+}
 
 @Composable
 fun EntrySiswaBody(
