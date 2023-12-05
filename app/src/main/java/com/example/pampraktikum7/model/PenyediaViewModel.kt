8f2fd53b3f6e71ad9200d650d75d81de011252fd
sarahannata <sarahannata@gmail.com>
2023-12-06 01:04:46 +0700

    create object PenyediaViewModel

---
+++ app/src/main/java/com/example/pampraktikum7/model/PenyediaViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.pampraktikum7.model
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.pampraktikum7.AplikasiSiswa
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory{
+        initializer{
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer{
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+/**
+ * Fungsi ekstensi query untuk object [Application] dan mengembalikan sebuah instance dari [AplikasiSiswa].
+ */
+
+fun CreationExtras.aplikasiSiswa():AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
