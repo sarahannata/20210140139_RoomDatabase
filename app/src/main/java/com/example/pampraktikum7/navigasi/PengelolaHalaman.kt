c8d5164ae64a2e0ca2a9161bada480f75543bbe6
sarahannata <sarahannata@gmail.com>
2023-12-06 19:40:09 +0700

    add fun HostNavigasi to PengelolaHalaman.kt

--- app/src/main/java/com/example/pampraktikum7/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pampraktikum7/navigasi/PengelolaHalaman.kt
@@ -11,6 +11,8 @@ import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.pampraktikum7.R
 
@@ -34,5 +36,14 @@ fun SiswaTopAppBar(
                     )
                 }
             }
-        })
+        }
+    )
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+
 }
