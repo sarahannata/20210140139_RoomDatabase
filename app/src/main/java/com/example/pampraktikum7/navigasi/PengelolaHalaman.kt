4a414596632697abbb0e379f7b4e5df40b89e47c
sarahannata <sarahannata@gmail.com>
2023-12-06 19:43:30 +0700

    add NavHost to fun HostNavigasi at PengelolaHalaman.kt

--- app/src/main/java/com/example/pampraktikum7/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pampraktikum7/navigasi/PengelolaHalaman.kt
@@ -13,8 +13,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.pampraktikum7.R
+import com.example.pampraktikum7.ui.halaman.DestinasiEntry
+import com.example.pampraktikum7.ui.halaman.DestinasiHome
+import com.example.pampraktikum7.ui.halaman.HomeScreen
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -45,5 +50,8 @@ fun HostNavigasi(
     navController: NavHostController,
     modifier: Modifier = Modifier
 ){
+    NavHost(navController = navController, startDestination = DestinasiHome.route, modifier = Modifier)
+    {
 
+    }
 }
