434f30df087a4567aeb95752862ea279da08bd5e
sarahannata <sarahannata@gmail.com>
2023-12-06 19:45:46 +0700

    add composable DestinasiEntry at NavHost

--- app/src/main/java/com/example/pampraktikum7/navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pampraktikum7/navigasi/PengelolaHalaman.kt
@@ -19,6 +19,7 @@ import androidx.navigation.compose.rememberNavController
 import com.example.pampraktikum7.R
 import com.example.pampraktikum7.ui.halaman.DestinasiEntry
 import com.example.pampraktikum7.ui.halaman.DestinasiHome
+import com.example.pampraktikum7.ui.halaman.EntrySiswaScreen
 import com.example.pampraktikum7.ui.halaman.HomeScreen
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -52,6 +53,11 @@ fun HostNavigasi(
 ){
     NavHost(navController = navController, startDestination = DestinasiHome.route, modifier = Modifier)
     {
-
+        composable(DestinasiHome.route){
+            HomeScreen(navigateToItemEntry = {navController.navigate(DestinasiEntry.route)})
+        }
+        composable(DestinasiEntry.route){
+            EntrySiswaScreen(navigateBack = {navController.popBackStack()})
+        }
     }
 }
